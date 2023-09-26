916393a07108998327aea2d1bb6d7dad74daadf7
chadjaa-sofiane <sofianne012@gmail.com>
2023-09-26 20:24:32 +0100

    fix the sendRequest not called asynchorosly.

--- background/background.js
+++ background/background.js
@@ -1,17 +1,17 @@
-chrome.runtime.onMessage.addListener(async (message, _, sendResponse) => {
+const recieveData = async (message, callback) => {
+  const response = await fetch(message.url, {
+    method: message.method,
+    headers: message.headers,
+    body: message.body,
+  });
+  const data = await response.json();
+  callback(data);
+};
+
+chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
   switch (message.type) {
     case "fetch":
-      try {
-        const response = await fetch(message.url, {
-          method: message.method,
-          headers: message.headers,
-          body: message.body,
-        });
-        const data = await response.json();
-        sendResponse(data);
-        break;
-      } catch (error) {
-        console.log(error);
-      }
+      recieveData(message, sendResponse);
+      return true;
   }
 });
