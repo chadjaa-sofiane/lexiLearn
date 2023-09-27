da5e5d24d4353279657c69de7f339317610c16d2
chadjaa-sofiane <sofianne012@gmail.com>
2023-09-27 17:06:04 +0100

    turn the main function into a module file.

---
+++ scripts/content.js
@@ -0,0 +1,12 @@
+"use strict";
+const script = document.createElement("script");
+
+script.setAttribute("type", "module");
+script.setAttribute("src", chrome.runtime.getURL("scripts/main.js"));
+
+const head =
+  document.head ||
+  document.getElementsByTagName("head")[0] ||
+  document.documentElement;
+
+head.insertBefore(script, head.lastChild);
