e803ec05ae2bb0a6af7a3793e37d0577c5d178eb
chadjaa-sofiane <sofianne012@gmail.com>
2023-09-29 21:29:20 +0100

    handle the new sentence result.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import { createWordInfo } from "../components/word_info.js";
+import { createSentenceInfo } from "../components/sentence_info.js";
 const inputForm = document.getElementById("input_form");
 const loadingSvg = document.getElementById("loading_svg");
 const resultContainer = document.getElementById("result_container");
@@ -8,21 +9,44 @@ inputForm.addEventListener("submit", (e) => {
   const formData = new FormData(e.target);
   const formProps = Object.fromEntries(formData);
   loadingSvg.style.display = "block";
-
-  chrome.runtime.sendMessage(
-    {
-      type: "fetch",
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      url: "http://localhost:3000/gpt/word",
-      body: JSON.stringify({
-        word: formProps.text_input,
-      }),
-    },
-    (result) => {
-      resultContainer.innerHTML = "";
-      loadingSvg.style.display = "none";
-      resultContainer.innerHTML = createWordInfo(result.data.data, formProps.text_input);
-    }
-  );
+  if (formProps.text_input.split(" ").length === 1) {
+    chrome.runtime.sendMessage(
+      {
+        type: "fetch",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        url: "http://localhost:3000/gpt",
+        body: JSON.stringify({
+          text: formProps.text_input,
+          type: "word",
+        }),
+      },
+      (result) => {
+        resultContainer.innerHTML = "";
+        loadingSvg.style.display = "none";
+        resultContainer.innerHTML = createWordInfo(
+          result.data.data,
+          formProps.text_input
+        );
+      }
+    );
+  } else {
+    chrome.runtime.sendMessage(
+      {
+        type: "fetch",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        url: "http://localhost:3000/gpt",
+        body: JSON.stringify({
+          text: formProps.text_input,
+          type: "sentence",
+        }),
+      },
+      (result) => {
+        resultContainer.innerHTML = "";
+        loadingSvg.style.display = "none";
+        resultContainer.innerHTML = createSentenceInfo(result.data.data);
+      }
+    );
+  }
 });
