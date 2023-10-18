ca2af9c415fd323cb14feabee5b25af5e8ca0683
chadjaa-sofiane <sofianne012@gmail.com>
2023-10-18 14:12:27 +0100

    make the viewContainer draggable.

--- client/src/scripts/content.ts
+++ client/src/scripts/content.ts
@@ -1,13 +1,18 @@
 import { getGPTResult } from "./api/getGPTResult";
 const viewContainer = document.createElement("div");
-viewContainer.style.position = "absolute";
+viewContainer.style.position = "fixed";
 viewContainer.classList.add("view_container");
+
 const resultContainer = document.createElement("div");
 resultContainer.classList.add("text_info_container");
 viewContainer.appendChild(resultContainer);
-// resultContainer.style.height = "auto";
+
+const wordElement = document.createElement("div");
+wordElement.style.fontWeight = "bold";
+wordElement.style.fontSize = "1.4em";
 
 let selected = false;
+let open = false;
 let text = "";
 let x: number;
 let y: number;
@@ -24,7 +29,7 @@ document.addEventListener("mouseup", (e) => {
   y = e.clientY;
   element.style.top = `${y}px`;
   element.style.left = `${x}px`;
-  if (selected) {
+  if (selected && !open) {
     document.body.appendChild(element);
   }
 });
@@ -42,8 +47,38 @@ document.addEventListener("selectionchange", () => {
 
 element.addEventListener("mousedown", (e) => {
   e.stopPropagation();
-  viewContainer.style.top = `${y}px`;
-  viewContainer.style.left = `${x}px`;
+  open = true;
+  resultContainer.innerHTML = "";
+  wordElement.innerHTML = text;
+  resultContainer.appendChild(wordElement);
+  viewContainer.style.top = `${y - 200}px`;
+  viewContainer.style.left = `${x + 40}px`;
   document.body.appendChild(viewContainer);
   getGPTResult(resultContainer, text);
+});
+
+let isDragging = false;
+let initialX: number;
+let initialY: number;
+
+viewContainer.addEventListener("mousedown", (e) => {
+  e.stopPropagation();
+  isDragging = true;
+  initialX = e.clientX - viewContainer.getBoundingClientRect().left;
+  initialY = e.clientY - viewContainer.getBoundingClientRect().top;
+  viewContainer.style.cursor = "grabbing";
+  viewContainer.style.border = "2px solid blue"
+});
+
+viewContainer.addEventListener("mouseup", () => {
+  viewContainer.style.border = "none"
+  isDragging = false;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    e.preventDefault();
+    viewContainer.style.left = e.clientX - initialX + "px";
+    viewContainer.style.top = e.clientY - initialY + "px";
+  }
 });
